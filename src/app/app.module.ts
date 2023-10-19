503706d0878e688d5c73a47295eb0b01654dc334
zafo351 <nicolacho2727@gmail.com>
2023-10-19 09:41:50 -0500

    front

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IngresoInformacionComponent } from './ingreso-informacion/ingreso-informacion.component';
+import { ResumenComponent } from './resumen/resumen.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IngresoInformacionComponent,
+    ResumenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
